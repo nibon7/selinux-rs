68ca28bfebe5552bc8512157d30bd5807b78157c
nibon7 <nibon7@163.com>
2020-09-22 13:18:49 +0800

    build static libselinux
    Signed-off-by: nibon7 <nibon7@163.com>

--- selinux-sys/build.rs
+++ selinux-sys/build.rs
@@ -1,16 +1,55 @@
 use bindgen;
 use pkg_config::probe_library;
 use std::env;
-use std::path::PathBuf;
+use std::ffi::OsStr;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
 fn main() {
-    probe_library("libselinux")
-        .expect("can't find libselinux, please install libselinux-devel or libselinux-dev");
-    println!("cargo:return-if-changed=build.rs");
+    println!("cargo:return-if-env-changed=SYSTEM_LIBSELINUX");
+
+    if use_system_libselinux() {
+        probe_library("libselinux")
+            .expect("can't find libselinux, please install libselinux-devel or libselinux-dev");
+        println!("cargo:return-if-changed=build.rs");
+    }
+
+    if !Path::new("selinux/.git").exists() {
+        Command::new("git")
+            .args(&["submodule", "update", "--init", "--recursive"])
+            .spawn()
+            .unwrap();
+    }
+
+    let output_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    let mut cfg = cc::Build::new();
+    for c in glob::glob("selinux/libselinux/src/*.c")
+        .unwrap()
+        .filter(|p| match p {
+            Ok(path) => {
+                // filter out audit2why.c and label_backends_androi.c
+                path.file_name() != Some(OsStr::new("audit2why.c"))
+                    && path.file_name() != Some(OsStr::new("label_backends_android.c"))
+            }
+            _ => false,
+        })
+    {
+        cfg.file(&c.unwrap());
+    }
+
+    cfg.include("selinux/libselinux/include")
+        .include("selinux/libsepol/include")
+        .define("NO_ANDROID_BACKEND", None)
+        .define("_GNU_SOURCE", None)
+        .cargo_metadata(true)
+        .out_dir(&output_dir)
+        .compile("libselinux.a");
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .default_enum_style(bindgen::EnumVariation::ModuleConsts)
+        .ctypes_prefix("::libc")
         .whitelist_function(".*con")
         .whitelist_function(".*conary")
         .whitelist_function(".*con_raw")
@@ -18,6 +57,7 @@ fn main() {
         .whitelist_function("security_.*")
         .whitelist_function(".*matchpathcon.*")
         .whitelist_function(".*context.*")
+        .clang_arg("-Iselinux/libselinux/include")
         .derive_debug(false)
         .derive_eq(true)
         .generate()
@@ -28,4 +68,11 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+}
+
+fn use_system_libselinux() -> bool {
+    match env::var("SYSTEM_LIBSELINUX") {
+        Ok(v) if v == "1" => true,
+        _ => false,
+    }
 }
--- selinux-sys/wrapper.h
+++ selinux-sys/wrapper.h
@@ -1,2 +1,7 @@
-#include <selinux/selinux.h>
+#include <selinux/avc.h>
 #include <selinux/context.h>
+#include <selinux/get_context_list.h>
+#include <selinux/get_default_type.h>
+#include <selinux/label.h>
+#include <selinux/restorecon.h>
+#include <selinux/selinux.h>
