c642c60764bfa3a82bceb3371468dc015ef5f0d4
Qiu Wenbo <qiuwenbo@kylinos.com.cn>
2020-09-24 23:05:53 +0800

    context: remove SCon as it is not used anymore
    Signed-off-by: Qiu Wenbo <qiuwenbo@kylinos.com.cn>

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,10 @@ extern crate selinux_macros;
 
 mod context;
 mod error;
-mod scon;
 mod utils;
 
 pub use context::*;
 pub use error::*;
-pub use scon::*;
 pub use utils::*;
 
 pub type Result<T> = std::result::Result<T, SeError>;
