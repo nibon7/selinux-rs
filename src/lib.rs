da6fe05265f4678c6faf18881200bd115c118f87
Qiu Wenbo <qiuwenbo@kylinos.com.cn>
2020-09-25 21:48:01 +0800

    error: use Error::InvalidPath to wrap NulError
    Currently, we are not using other types of error such as Utf8Error and
    std::io::Error. So we can safely remove them.

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,11 @@ pub use utils::*;
 pub type Result<T> = std::result::Result<T, Error>;
 
 use std::ffi::NulError;
-use std::io::Error as IoError;
-use std::path::PathBuf;
-use std::str::Utf8Error;
 
 #[derive(Debug)]
 pub enum Error {
-    NulErr(NulError),
-    Utf8Err(Utf8Error),
-    IoErr(IoError),
     Generic,
-    InvalidPath(PathBuf),
+    InvalidPath(Vec<u8>),
     NoSpace,
     QuotaEnforcement,
     NotSupported,
@@ -26,18 +20,6 @@ pub enum Error {
 
 impl From<NulError> for Error {
     fn from(e: NulError) -> Self {
-        Error::NulErr(e)
-    }
-}
-
-impl From<Utf8Error> for Error {
-    fn from(e: Utf8Error) -> Self {
-        Error::Utf8Err(e)
-    }
-}
-
-impl From<IoError> for Error {
-    fn from(e: IoError) -> Self {
-        Error::IoErr(e)
+        Error::InvalidPath(e.into_vec())
     }
 }
